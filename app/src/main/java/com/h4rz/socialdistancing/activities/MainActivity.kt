7ee39776dd340a1f4e5713d231407a310f6feb29
activesince93 <darshan@unifynd.com>
2020-04-28 12:18:01 +0530

    Add shortcut to disable battery saver (Xiaomi only)

--- app/src/main/java/com/h4rz/socialdistancing/activities/MainActivity.kt
+++ app/src/main/java/com/h4rz/socialdistancing/activities/MainActivity.kt
@@ -2,22 +2,27 @@ package com.h4rz.socialdistancing.activities
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.h4rz.socialdistancing.R
 import com.h4rz.socialdistancing.application.MyApplication
 import com.h4rz.socialdistancing.utility.Constants.BLUETOOTH_INTENT_REQUEST_CODE
 import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var context: Context
+    private var isXiaomiDevice = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +31,36 @@ class MainActivity : BaseActivity() {
         askForPermissions()
     }
 
+    private fun checkIfXiaomiDevice() {
+        if(Build.MANUFACTURER.toLowerCase() == "xiaomi")
+            btnDisableBatterySaver.visibility = View.VISIBLE
+        else
+            btnDisableBatterySaver.visibility = View.GONE
+    }
+
+    private fun disableBatterySaverForMiDevices() {
+        if(Build.MANUFACTURER.toLowerCase() == "xiaomi") {
+            try {
+                val intent = Intent()
+                intent.component = ComponentName(
+                    "com.miui.powerkeeper",
+                    "com.miui.powerkeeper.ui.HiddenAppsConfigActivity"
+                )
+                intent.putExtra("package_name", packageName)
+                intent.putExtra("package_label", getText(R.string.app_name))
+                startActivity(intent)
+            } catch (anfe: ActivityNotFoundException) {
+                anfe.printStackTrace()
+            }
+        }
+    }
+
     private fun initializeVariables() {
         context = this
+        btnDisableBatterySaver.setOnClickListener(this)
+
+        // Show Battery saver button only when manufacturer is XIAOMI
+        checkIfXiaomiDevice()
     }
 
     private fun askForPermissions() = runWithPermissions(
@@ -120,6 +153,14 @@ class MainActivity : BaseActivity() {
         super.onResume()
         (applicationContext as MyApplication).setMainActivity(this)
         (applicationContext as MyApplication).enableMonitoring()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btnDisableBatterySaver -> {
+                disableBatterySaverForMiDevices()
+            }
+        }
     }
 
 }
